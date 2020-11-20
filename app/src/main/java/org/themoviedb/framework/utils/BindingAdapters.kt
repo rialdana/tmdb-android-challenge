839fda89da2fa954811b58207fc943eda78b12f7
Ricardo Cardona <rcardona@applaudostudios.com>
2020-11-20 15:18:40 -0600

    :tada: Added lottie to display loading and error animations when fetching data

--- app/src/main/java/org/themoviedb/framework/utils/BindingAdapters.kt
+++ app/src/main/java/org/themoviedb/framework/utils/BindingAdapters.kt
@@ -1,8 +1,10 @@
 package org.themoviedb.framework.utils
 
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
 
 @Suppress("UNCHECKED_CAST")
@@ -20,5 +22,50 @@ fun loadImage(imageView: ImageView, url: String?) {
     url?.let {
         Glide.with(imageView.context).load(BASE_IMAGE_URL + url)
             .into(imageView)
+    }
+}
+
+@BindingAdapter("app:showOnSuccess")
+fun showOnSuccess(view: View, apiCallStatus: ApiCallStatus?) {
+    apiCallStatus?.let {
+        if (apiCallStatus == ApiCallStatus.SUCCESS) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.GONE
+        }
+    }
+}
+
+@BindingAdapter("app:showOnLoading")
+fun showOnLoading(lottie: LottieAnimationView, apiCallStatus: ApiCallStatus?) {
+    apiCallStatus?.let {
+        if (apiCallStatus == ApiCallStatus.LOADING) {
+            lottie.apply {
+                visibility = View.VISIBLE
+                playAnimation()
+            }
+        } else {
+            lottie.apply {
+                visibility = View.GONE
+                cancelAnimation()
+            }
+        }
+    }
+}
+
+@BindingAdapter("app:showOnError")
+fun showOnError(lottie: LottieAnimationView, apiCallStatus: ApiCallStatus?) {
+    apiCallStatus?.let {
+        if (apiCallStatus == ApiCallStatus.ERROR) {
+            lottie.apply {
+                visibility = View.VISIBLE
+                playAnimation()
+            }
+        } else {
+            lottie.apply {
+                visibility = View.GONE
+                cancelAnimation()
+            }
+        }
     }
 }
--- app/src/main/java/org/themoviedb/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/org/themoviedb/ui/movies/MoviesViewModel.kt
@@ -1,21 +1,21 @@
 package org.themoviedb.ui.movies
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.themobiedb.model.movies.Movies
 import org.themobiedb.data.onFailure
 import org.themobiedb.data.onSuccess
 import org.themobiedb.interactors.GetPopularMovies
 import org.themobiedb.interactors.GetTopRatedMovies
+import org.themobiedb.model.movies.Movies
+import org.themoviedb.framework.utils.ApiCallStatus
+import org.themoviedb.ui.base.BaseViewModel
 
 class MoviesViewModel(
     private val getTopRatedMovies: GetTopRatedMovies,
     private val getPopularMovies: GetPopularMovies
-) : ViewModel() {
+) : BaseViewModel() {
 
     private val _topRatedMovies = MutableLiveData<Movies>()
     val topRatedMovies: LiveData<Movies>
@@ -26,23 +26,36 @@ class MoviesViewModel(
         get() = _popularMovies
 
     init {
+        loadTopRatedMovies()
+        loadPopularMovies()
+    }
 
+    private fun loadTopRatedMovies() {
         viewModelScope.launch {
+            apiCallOne.value = ApiCallStatus.LOADING
+
             getTopRatedMovies()
                 .onSuccess {
                     _topRatedMovies.value = it
+                    apiCallOne.value = ApiCallStatus.SUCCESS
                 }
                 .onFailure {
-                    Log.i("MoviesViewModel", it.message.toString())
+                    apiCallOne.value = ApiCallStatus.ERROR
                 }
+        }
+    }
 
+    private fun loadPopularMovies() {
+        viewModelScope.launch {
+            apiCallTwo.value = ApiCallStatus.LOADING
 
             getPopularMovies()
                 .onSuccess {
                     _popularMovies.value = it
+                    apiCallTwo.value = ApiCallStatus.SUCCESS
                 }
                 .onFailure {
-                    Log.i("MoviesViewModel", it.message.toString())
+                    apiCallTwo.value = ApiCallStatus.ERROR
                 }
         }
     }
